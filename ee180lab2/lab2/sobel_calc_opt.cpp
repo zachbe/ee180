167f1d236db659934c23f3c25a6ef75ea9f257c9
Vinh Nguyen <vnguyen5@stanford.edu>
2018-01-30 02:58:59 +0000

    uncommented vectorization in sobel_calc; sobel_calc_opt is a backup for comparing with sobel_calc_base

--- ee180lab2/lab2/sobel_calc_opt.cpp
+++ ee180lab2/lab2/sobel_calc_opt.cpp
@@ -16,23 +16,23 @@ void grayScale(Mat& img, Mat& img_gray_out)
   uint8_t*arr_red =  img.data;
   unsigned char* output_arr = img_gray_out.data;
   // Convert to grayscale
-//  for (int i = 0; i < rows*cols; i++) {
-//	output_arr[i] = .114*arr_red[STEP1*i] + .587*arr_red[STEP1*i + 1] + .299*arr_red[STEP1*i+2];
-  //}
-
+  for (int i = 0; i < rows*cols; i++) {
+	output_arr[i] = .114*arr_red[STEP1*i] + .587*arr_red[STEP1*i + 1] + .299*arr_red[STEP1*i+2];
+  }
+/*
   for (int i=0; i<rows*cols/8; i++) {
           uint8x8x3_t vec = vld3_u8(arr_red + 24*i);
           uint8x8_t vec_output = vshr_n_u8(vec.val[0],3);
 	  //vec_output = vsub_u8(vec_output,vshr_n_u8(vec.val[0],6)); //.109 (error:.006)
           vec_output = vadd_u8(vec_output,vshr_n_u8(vec.val[1],1));
 	  //vec_output = vadd_u8(vec_output, vshr_n_u8(vec.val[1],4));
-	  vec_output = vadd_u8(vec_output, vshr_n_u8(vec.val[1],5));//.593 (error .006)
+	  //vec_output = vadd_u8(vec_output, vshr_n_u8(vec.val[1],5));//.593 (error .006)
 	  vec_output = vadd_u8(vec_output, vshr_n_u8(vec.val[2],2));
 	  //vec_output = vadd_u8(vec_output, vshr_n_u8(vec.val[2],4));
-	  vec_output = vsub_u8(vec_output, vshr_n_u8(vec.val[2],6));//.296 (error .003) 
+	  //vec_output = vsub_u8(vec_output, vshr_n_u8(vec.val[2],6));//.296 (error .003) 
           vst1_u8(output_arr + 8*i, vec_output);
    }
-
+*/
 }
 
 /*******************************************
@@ -56,7 +56,7 @@ void sobelCalc(Mat& img_gray, Mat& img_sobel_out)
   int rows = img_gray.rows;
   int cols = img_gray.cols;
   uint8_t* baseptr = img_gray.data;
-  
+  /*
   for (int i = 1; i<rows; i++) {
 	for(int j = 0; j<cols/8; j++) {
 		uint8x8_t vtopleft = vld1_u8(baseptr + IMG_WIDTH*(i-1) + (8*(j)));
@@ -78,7 +78,7 @@ void sobelCalc(Mat& img_gray, Mat& img_sobel_out)
 		sobel = vsubw_u8(sobel, vcentright);
 		sobel = vsubw_u8(sobel, vbotright);
 		sobel =  vsubw_u8(sobel, vbotright);
-		/*uint16x8_t sobelx = vaddl_u8(vtopleft,vtopright);
+		uint16x8_t sobelx = vaddl_u8(vtopleft,vtopright);
 		sobelx = vaddw_u8(sobelx, vtopcent);
 		sobelx = vaddw_u8(sobelx, vtopcent);
 		sobelx = vsubw_u8(sobelx, vbotcent);
@@ -92,14 +92,14 @@ void sobelCalc(Mat& img_gray, Mat& img_sobel_out)
 		sobely = vsubw_u8(sobely, vcentright);
 		sobely = vsubw_u8(sobely, vtopright);
 		sobely = vsubw_u8(sobely, vbotright);
-		//uint16x8_t sobel = sobelx + sobely;*/  
-		vst1_u8(img_sobel_out.data + IMG_WIDTH*i + 8*j, vreinterpret_u8_s8(vqmovn_s16(vabsq_s16(vreinterpretq_s16_u16(sobel)))));
-		//vst1_u8(img_outx.data + IMG_WIDTH*i + 8*j + 1, vreinterpret_u8_s8(vqmovn_s16(vabsq_s16(vreinterpretq_s16_u16(sobelx)))));
-	//	vst1_u8(img_outy.data + IMG_WIDTH*i + 8*j + 1, vreinterpret_u8_s8(vqmovn_s16(vabsq_s16(vreinterpretq_s16_u16(sobely)))));
+		//uint16x8_t sobel = sobelx + sobely;  
+		//vst1_u8(img_sobel_out.data + IMG_WIDTH*i + 8*j, vreinterpret_u8_s8(vqmovn_s16(vabsq_s16(vreinterpretq_s16_u16(sobel)))));
+		vst1_u8(img_outx.data + IMG_WIDTH*i + 8*j + 1, vreinterpret_u8_s8(vqmovn_s16(vabsq_s16(vreinterpretq_s16_u16(sobelx)))));
+		vst1_u8(img_outy.data + IMG_WIDTH*i + 8*j + 1, vreinterpret_u8_s8(vqmovn_s16(vabsq_s16(vreinterpretq_s16_u16(sobely)))));
 	}
 }
 
-/*
+*/
   for (int i=1; i<img_gray.rows; i++) {
     for (int j=1; j<img_gray.cols; j++) {
       sobel = abs(img_gray.data[IMG_WIDTH*(i-1) + (j-1)] -
@@ -139,5 +139,5 @@ void sobelCalc(Mat& img_gray, Mat& img_sobel_out)
       sobel = (sobel > 255) ? 255 : sobel;
       img_sobel_out.data[IMG_WIDTH*(i) + j] = sobel;
     }
-  }*/
+  }
 }
