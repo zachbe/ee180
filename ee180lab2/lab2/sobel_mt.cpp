a71259f5e51459e9151b05fdfbb7eb3d604a6fc4
zbelateche <zachbela@stanford.edu>
2018-02-04 15:16:53 -0800

    multithreading progress

--- ee180lab2/lab2/sobel_mt.cpp
+++ ee180lab2/lab2/sobel_mt.cpp
@@ -21,7 +21,7 @@ using namespace cv;
 static ofstream results_file;
 
 // Define image mats to pass between function calls
-static Mat img_gray, img_sobel;
+static Mat img_gray, img_sobel, img_gray_top, img_gray_bot, img_sobel_top, img_sobel_bot, topsrc, botsrc;
 static float total_fps, total_ipc, total_epf;
 static float gray_total, sobel_total, cap_total, disp_total;
 static float sobel_ic_total, sobel_l1cm_total;
@@ -34,6 +34,9 @@ static float sobel_ic_total, sobel_l1cm_total;
  *   sobelCalc module, and displays the returned Sobel filtered image. This
  *   function processes NUM_ITER frames.
  ********************************************/
+
+pthread_barrier_t startSobel;
+
 void *runSobelMT(void *ptr)
 {
   // Set up variables for computing Sobel
@@ -54,83 +57,108 @@ void *runSobelMT(void *ptr)
 
   // For now, we just kill the second thread. It's up to you to get it to compute
   // the other half of the image.
-  if (myID != thread0_id) {
-    pthread_barrier_wait(&endSobel);
-    return NULL;
-  }
-
-  pc_init(&perf_counters, 0);
 
   // Start algorithm
   CvCapture* video_cap;
+  if (myID == thread0_id) {
+    pc_init(&perf_counters, 0);
 
-  if (opts.webcam) {
-    video_cap = cvCreateCameraCapture(-1);
-  } else {
+    if (opts.webcam) {
+      video_cap = cvCreateCameraCapture(-1);
+    } else {
+      video_cap = cvCreateFileCapture(opts.videoFile);
+    }
+
     video_cap = cvCreateFileCapture(opts.videoFile);
+    cvSetCaptureProperty(video_cap, CV_CAP_PROP_FRAME_WIDTH, IMG_WIDTH);
+    cvSetCaptureProperty(video_cap, CV_CAP_PROP_FRAME_HEIGHT, IMG_HEIGHT);
+
   }
-
-  video_cap = cvCreateFileCapture(opts.videoFile);
-  cvSetCaptureProperty(video_cap, CV_CAP_PROP_FRAME_WIDTH, IMG_WIDTH);
-  cvSetCaptureProperty(video_cap, CV_CAP_PROP_FRAME_HEIGHT, IMG_HEIGHT);
-
   // Keep track of the frames
   int i = 0;
 
   while (1) {
-    // Allocate memory to hold grayscale and sobel images
-    img_gray = Mat(IMG_HEIGHT, IMG_WIDTH, CV_8UC1);
-    img_sobel = Mat(IMG_HEIGHT, IMG_WIDTH, CV_8UC1);
+    if (myID == thread0_id) {
+      // Allocate memory to hold grayscale and sobel images
+      img_gray_top = Mat(IMG_HEIGHT/2, IMG_WIDTH, CV_8UC1);
+      img_sobel_top = Mat(IMG_HEIGHT/2, IMG_WIDTH, CV_8UC1);
+      img_gray_bot = Mat(IMG_HEIGHT - IMG_HEIGHT/2, IMG_WIDTH, CV_8UC1);
+      img_sobel_bot = Mat(IMG_HEIGHT - IMG_HEIGHT/2, IMG_WIDTH, CV_8UC1);
+      img_sobel = Mat(IMG_HEIGHT, IMG_WIDTH, CV_8UC1);
 
-    pc_start(&perf_counters);
-    src = cvQueryFrame(video_cap);
-    pc_stop(&perf_counters);
+      pc_start(&perf_counters);
+      src = cvQueryFrame(video_cap);
 
-    cap_time = perf_counters.cycles.count;
-    sobel_l1cm = perf_counters.l1_misses.count;
-    sobel_ic = perf_counters.ic.count;
+      //split frame in two for each worker
+      Rect toprec(0, 0, src.cols, src.rows / 2);
+      Rect botrec(0, toprec.height, src.cols, src.rows - toprec.height);
+      topsrc = src(toprec);
+      botsrc = src(botrec);
+
+      pc_stop(&perf_counters);
+
+      cap_time = perf_counters.cycles.count;
+      sobel_l1cm = perf_counters.l1_misses.count;
+      sobel_ic = perf_counters.ic.count;
+    }
+
+    pthread_barrier_wait(&startSobel);
 
     // LAB 2, PART 2: Start parallel section
     pc_start(&perf_counters);
-    grayScale(src, img_gray);
+    if (myID == thread0_id){
+      grayScale(topsrc, img_gray_top);
+    }
+    else{ grayScale(botsrc, img_gray_bot); }
     pc_stop(&perf_counters);
 
-    gray_time = perf_counters.cycles.count;
-    sobel_l1cm += perf_counters.l1_misses.count;
-    sobel_ic += perf_counters.ic.count;
+    if (myID == thread0_id){
+      gray_time = perf_counters.cycles.count;
+      sobel_l1cm += perf_counters.l1_misses.count;
+      sobel_ic += perf_counters.ic.count;
+    }
 
     pc_start(&perf_counters);
-    sobelCalc(img_gray, img_sobel);
+    if (myID == thread0_id){
+      grayScale(img_gray_top, img_sobel_top);
+    }
+    else{ grayScale(img_gray_bot, img_sobel_bot); }
     pc_stop(&perf_counters);
 
-    sobel_time = perf_counters.cycles.count;
-    sobel_l1cm += perf_counters.l1_misses.count;
-    sobel_ic += perf_counters.ic.count;
+    if (myID == thread0_id){
+      sobel_time = perf_counters.cycles.count;
+      sobel_l1cm += perf_counters.l1_misses.count;
+      sobel_ic += perf_counters.ic.count;
+
+      //join the two mats
+      vconcat(img_sobel_top, img_sobel_bot, img_sobel);
+    
     // LAB 2, PART 2: End parallel section
 
-    pc_start(&perf_counters);
-    namedWindow(top, CV_WINDOW_AUTOSIZE);
-    imshow(top, img_sobel);
-    pc_stop(&perf_counters);
+      pc_start(&perf_counters);
+      namedWindow(top, CV_WINDOW_AUTOSIZE);
+      imshow(top, img_sobel);
+      pc_stop(&perf_counters);
 
-    disp_time = perf_counters.cycles.count;
-    sobel_l1cm += perf_counters.l1_misses.count;
-    sobel_ic += perf_counters.ic.count;
+      disp_time = perf_counters.cycles.count;
+      sobel_l1cm += perf_counters.l1_misses.count;
+      sobel_ic += perf_counters.ic.count;
 
-    cap_total += cap_time;
-    gray_total += gray_time;
-    sobel_total += sobel_time;
-    sobel_l1cm_total += sobel_l1cm;
-    sobel_ic_total += sobel_ic;
-    disp_total += disp_time;
-    total_fps += PROC_FREQ/float(cap_time + disp_time + gray_time + sobel_time);
-    total_ipc += float(sobel_ic/float(cap_time + disp_time + gray_time + sobel_time));
-    i++;
+      cap_total += cap_time;
+      gray_total += gray_time;
+      sobel_total += sobel_time;
+      sobel_l1cm_total += sobel_l1cm;
+      sobel_ic_total += sobel_ic;
+      disp_total += disp_time;
+      total_fps += PROC_FREQ/float(cap_time + disp_time + gray_time + sobel_time);
+      total_ipc += float(sobel_ic/float(cap_time + disp_time + gray_time + sobel_time));
+      i++;
 
-    // Press q to exit
-    char c = cvWaitKey(10);
-    if (c == 'q' || i >= opts.numFrames) {
-      break;
+      // Press q to exit
+      char c = cvWaitKey(10);
+      if (c == 'q' || i >= opts.numFrames) {
+        break;
+      }
     }
   }
 
