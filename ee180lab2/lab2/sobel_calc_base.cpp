2dee97834e615c5290d47f251d7ab413f01480d6
invictus1123 <vnguyen5@stanford.edu>
2018-02-04 17:06:55 -0800

    add base sobel calc for debug and comparison

---
+++ ee180lab2/lab2/sobel_calc_base.cpp
@@ -0,0 +1,91 @@
+#include "opencv2/imgproc/imgproc.hpp"
+#include "sobel_alg.h"
+#include <arm_neon.h>
+using namespace cv;
+
+/*******************************************
+ * Model: grayScale
+ * Input: Mat img
+ * Output: None directly. Modifies a ref parameter img_gray_out
+ * Desc: This module converts the image to grayscale
+ ********************************************/
+void grayScale(Mat& img, Mat& img_gray_out)
+{
+  const int rows = IMG_HEIGHT;//640x480
+  const int cols = IMG_WIDTH;//step1 = 3 for 3 contiguous RGB values
+  uint8_t*arr_red =  img.data;
+  uint8_t*arr_green = arr_red + 1;
+  uint8_t*arr_blue = arr_red + 2;
+  uint8x8x3_t rgbvec;
+  float colorblue;
+  float colorgreen;
+  unsigned char* output_arr = img_gray_out.data;
+  // Convert to grayscale
+  float colorred;
+  for (int i=0; i<rows*cols; i++) {
+          colorred = .114*arr_red[STEP1*i];
+          colorgreen = .587*arr_green[STEP1*i]
+          colorblue =  .299*arr_blue[STEP1*i];
+          output_arr[i] = colorred+colorblue+colorgreen;
+      }
+   }
+}
+
+/*******************************************
+ * Model: sobelCalc
+ * Input: Mat img_in
+ * Output: None directly. Modifies a ref parameter img_sobel_out
+ * Desc: This module performs a sobel calculation on an image. It first
+ *  converts the image to grayscale, calculates the gradient in the x
+ *  direction, calculates the gradient in the y direction and sum it with Gx
+ *  to finish the Sobel calculation
+ ********************************************/
+void sobelCalc(Mat& img_gray, Mat& img_sobel_out)
+{
+  Mat img_outx = img_gray.clone();
+  Mat img_outy = img_gray.clone();
+
+  // Apply Sobel filter to black & white image
+  unsigned short sobel;
+
+  // Calculate the x convolution
+  for (int i=1; i<img_gray.rows; i++) {
+    for (int j=1; j<img_gray.cols; j++) {
+      sobel = abs(img_gray.data[IMG_WIDTH*(i-1) + (j-1)] -
+		  img_gray.data[IMG_WIDTH*(i+1) + (j-1)] +
+		  2*img_gray.data[IMG_WIDTH*(i-1) + (j)] -
+		  2*img_gray.data[IMG_WIDTH*(i+1) + (j)] +
+		  img_gray.data[IMG_WIDTH*(i-1) + (j+1)] -
+		  img_gray.data[IMG_WIDTH*(i+1) + (j+1)]);
+
+      sobel = (sobel > 255) ? 255 : sobel;
+      img_outx.data[IMG_WIDTH*(i) + (j)] = sobel;
+    }
+  }
+
+  // Calc the y convolution
+  for (int i=1; i<img_gray.rows; i++) {
+    for (int j=1; j<img_gray.cols; j++) {
+     sobel = abs(img_gray.data[IMG_WIDTH*(i-1) + (j-1)] -
+		   img_gray.data[IMG_WIDTH*(i-1) + (j+1)] +
+		   2*img_gray.data[IMG_WIDTH*(i) + (j-1)] -
+		   2*img_gray.data[IMG_WIDTH*(i) + (j+1)] +
+		   img_gray.data[IMG_WIDTH*(i+1) + (j-1)] -
+		   img_gray.data[IMG_WIDTH*(i+1) + (j+1)]);
+
+     sobel = (sobel > 255) ? 255 : sobel;
+
+     img_outy.data[IMG_WIDTH*(i) + j] = sobel;
+    }
+  }
+
+  // Combine the two convolutions into the output image
+  for (int i=1; i<img_gray.rows; i++) {
+    for (int j=1; j<img_gray.cols; j++) {
+      sobel = img_outx.data[IMG_WIDTH*(i) + j] +
+	img_outy.data[IMG_WIDTH*(i) + j];
+      sobel = (sobel > 255) ? 255 : sobel;
+      img_sobel_out.data[IMG_WIDTH*(i) + j] = sobel;
+    }
+  }
+}
