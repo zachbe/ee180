586c1f96e8fac9099e6c0ce0da8cc1befd9c50ad
zbelateche <zachbela@stanford.edu>
2018-02-28 14:30:05 -0800

    branch less thans

--- ee180lab3/starter/hw/hdl/verilog/mips/decode.v
+++ ee180lab3/starter/hw/hdl/verilog/mips/decode.v
@@ -257,11 +257,14 @@ module decode (
 //******************************************************************************
 
     wire isEqual = rs_data == rt_data;
-    wire isGTZ = rs_data > 32'b0;
+    wire isGTEZ = rs_data => 32'b0;
+    wire isLTEZ = rs_data <= 32'b0;
 
     assign jump_branch = |{isBEQ & isEqual,
                            isBNE & ~isEqual,
-                           isBGEZNL & isGTZ};
+                           isBGEZNL & isGTEZ,
+                           isBLEZ & isLTEZ,
+                           isBLTZNL & ~isGTEZ};
 
     assign jump_target = isJ;
     assign jump_reg = isJR;
