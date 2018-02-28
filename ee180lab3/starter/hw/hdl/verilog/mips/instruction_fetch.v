175336f16e5bb156e8fc662fc24d9b55329b2798
zbelateche <zachbela@stanford.edu>
2018-02-28 00:27:08 -0800

    learning parenthises 2

--- ee180lab3/starter/hw/hdl/verilog/mips/instruction_fetch.v
+++ ee180lab3/starter/hw/hdl/verilog/mips/instruction_fetch.v
@@ -23,10 +23,12 @@ module instruction_fetch (
     wire [31:0] pc_id_p4 = pc_id + 3'h4;
     wire [31:0] j_addr = {pc_id_p4[31:28], instr_id[25:0], 2'b0};
 
-    wire [31:0] pc_next =   jump_reg ? jr_pc : (
-                            jump_branch ? (pc_id_p4 + jr_pc) : (
-                            jump_target ? j_addr : (pc_id + 3'h4)
-                            ));
+    wire [31:0] pc_next =   jump_reg ? jr_pc : ((jump_target) ? j_addr : (pc + 3'h4));
+
+    // jump_reg ? jr_pc : (
+    //                         jump_branch ? (pc_id_p4 + jr_pc) : (
+    //                         jump_target ? j_addr : (pc_id + 3'h4)
+    //                         ));
 
     dffare #(32) pc_reg (.clk(clk), .r(rst), .en(en), .d(pc_next), .q(pc));
 
