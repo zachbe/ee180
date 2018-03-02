eb2d2a2cb5af809b9217bca0dbc8b072fd7badb3
zbelateche <zachbela@stanford.edu>
2018-03-01 20:08:56 -0800

    edited ll

--- ee180lab3/starter/hw/hdl/verilog/mips/mips_cpu.v
+++ ee180lab3/starter/hw/hdl/verilog/mips/mips_cpu.v
@@ -162,7 +162,7 @@ module mips_cpu (
 
     // needed for M stage
     wire [31:0] sc_result = {{31{1'b0}},(mem_sc_ex & mem_we_ex)};
-    wire [31:0] alu_sc_result_ex = mem_we_ex ? sc_result : alu_result_ex;   // TODO: Need to conditionally inject SC value
+    wire [31:0] alu_sc_result_ex = (mem_sc_ex & mem_we_ex) ? sc_result : alu_result_ex;   // TODO: Need to conditionally inject SC value
     dffare #(32) alu_result_ex2mem (.clk(clk), .r(rst), .en(en), .d(alu_sc_result_ex), .q(alu_result_mem));
 	//edits up to 2/25 (Vinh)
 	//formerly .d(1'b0), .q()
